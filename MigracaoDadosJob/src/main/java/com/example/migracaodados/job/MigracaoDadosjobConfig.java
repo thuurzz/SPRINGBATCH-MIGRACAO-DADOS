24c4132ac762943256b5b734ca1e0eaae3c1e9a7
Arthur Vinicius <thuur.vss@gmail.com>
2021-11-06 12:47:00 -0300

    Criação de leitores e escritores.

---
+++ MigracaoDadosJob/src/main/java/com/example/migracaodados/job/MigracaoDadosjobConfig.java
@@ -0,0 +1,5 @@
+package com.example.migracaodados.job;
+
+public class MigracaoDadosjobConfig {
+
+}
---
+++ MigracaoDadosJob/src/main/java/com/example/migracaodados/step/MigrarPessoasStepConfig.java
@@ -0,0 +1,5 @@
+package com.example.migracaodados.step;
+
+public class MigrarPessoasStepConfig {
+
+}
